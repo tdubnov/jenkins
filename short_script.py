3614f9008745399d152caf4234fefe79ca1d6c03
michaelmarcus <marcus.mika@gmail.com>
2020-06-14 15:00:58 +0300

    first commit

---
+++ short_script.py
@@ -0,0 +1,26 @@
+"""
+Short script calculating sqrt
+"""
+import sys
+import math
+
+def main():
+    args = sys.argv[1:]
+    try:
+        num = int(args[0])
+    except SyntaxError:
+        print('Please provide an integer number ./short_script.py number')
+        sys.exit(1)
+    except IndexError:
+        print('Please provide an integer number ./short_script.py number')
+        sys.exit(1)
+    except ValueError:
+        print('Please provide an integer number ./short_script.py number')
+        sys.exit(1)
+
+    for i in range(num):
+        print(i, math.sqrt(i))
+
+
+if __name__ == '__main__':
+    main()
